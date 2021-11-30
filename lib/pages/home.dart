26e53dea079b2c827a1d801802035d23f46a64f6
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 09:40:53 +0200

    full widget tree

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,9 +12,109 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home'),
+        title: const Text('Widget Tree'),
       ),
-      body: Container(),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Container(
+                      color: Colors.yellow,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                    const SizedBox(
+                      width: 32.0,
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Colors.amber,
+                        height: 40.0,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 32.0,
+                    ),
+                    Container(
+                      color: Colors.brown,
+                      height: 40.0,
+                      width: 40.0,
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 32.0,
+                ),
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Container(
+                          color: Colors.yellow,
+                          height: 60.0,
+                          width: 60.0,
+                        ),
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        Container(
+                          color: Colors.yellow,
+                          height: 40.0,
+                          width: 40.0,
+                        ),
+                        const SizedBox(
+                          height: 32.0,
+                        ),
+                        Container(
+                          color: Colors.brown,
+                          height: 20.0,
+                          width: 20.0,
+                        ),
+                        const Divider(),
+                        Row(
+                          children: [
+                            CircleAvatar(
+                              backgroundColor: Colors.lightGreen,
+                              radius: 100.0,
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    height: 100.0,
+                                    width: 100.0,
+                                    color: Colors.yellow,
+                                  ),
+                                  Container(
+                                    height: 60.0,
+                                    width: 60.0,
+                                    color: Colors.amber,
+                                  ),
+                                  Container(
+                                    height: 40.0,
+                                    width: 40.0,
+                                    color: Colors.brown,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Divider(),
+                        const Text('End of the Line'),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
