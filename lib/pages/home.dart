aa8c91c84bd541c5fae24c5846e7ebc830a1c71d
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-11-30 09:55:58 +0200

    refactor widget tree to a shallow tree using methods

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -20,101 +20,113 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                Row(
-                  children: [
-                    Container(
-                      color: Colors.yellow,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                    const SizedBox(
-                      width: 32.0,
-                    ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.amber,
-                        height: 40.0,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 32.0,
-                    ),
-                    Container(
-                      color: Colors.brown,
-                      height: 40.0,
-                      width: 40.0,
-                    ),
-                  ],
-                ),
+                _buildHorizontalRow(),
                 const SizedBox(
                   height: 32.0,
                 ),
-                Row(
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Container(
-                          color: Colors.yellow,
-                          height: 60.0,
-                          width: 60.0,
-                        ),
-                        const SizedBox(
-                          height: 32.0,
-                        ),
-                        Container(
-                          color: Colors.yellow,
-                          height: 40.0,
-                          width: 40.0,
-                        ),
-                        const SizedBox(
-                          height: 32.0,
-                        ),
-                        Container(
-                          color: Colors.brown,
-                          height: 20.0,
-                          width: 20.0,
-                        ),
-                        const Divider(),
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              backgroundColor: Colors.lightGreen,
-                              radius: 100.0,
-                              child: Stack(
-                                children: [
-                                  Container(
-                                    height: 100.0,
-                                    width: 100.0,
-                                    color: Colors.yellow,
-                                  ),
-                                  Container(
-                                    height: 60.0,
-                                    width: 60.0,
-                                    color: Colors.amber,
-                                  ),
-                                  Container(
-                                    height: 40.0,
-                                    width: 40.0,
-                                    color: Colors.brown,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Divider(),
-                        const Text('End of the Line'),
-                      ],
-                    ),
-                  ],
-                ),
+                _buildRowAndColumn(),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Row _buildRowAndColumn() {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              color: Colors.yellow,
+              height: 60.0,
+              width: 60.0,
+            ),
+            const SizedBox(
+              height: 32.0,
+            ),
+            Container(
+              color: Colors.yellow,
+              height: 40.0,
+              width: 40.0,
+            ),
+            const SizedBox(
+              height: 32.0,
+            ),
+            Container(
+              color: Colors.brown,
+              height: 20.0,
+              width: 20.0,
+            ),
+            const Divider(),
+            _buildRowAndStack(),
+            const Divider(),
+            const Text('End of the Line'),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Row _buildRowAndStack() {
+    return Row(
+      children: [
+        CircleAvatar(
+          backgroundColor: Colors.lightGreen,
+          radius: 100.0,
+          child: Stack(
+            children: [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                height: 60.0,
+                width: 60.0,
+                color: Colors.amber,
+              ),
+              Container(
+                height: 40.0,
+                width: 40.0,
+                color: Colors.brown,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _buildHorizontalRow() {
+    return Row(
+      children: [
+        Container(
+          color: Colors.yellow,
+          height: 40.0,
+          width: 40.0,
+        ),
+        const SizedBox(
+          width: 32.0,
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.amber,
+            height: 40.0,
+          ),
+        ),
+        const SizedBox(
+          width: 32.0,
+        ),
+        Container(
+          color: Colors.brown,
+          height: 40.0,
+          width: 40.0,
+        ),
+      ],
     );
   }
 }
